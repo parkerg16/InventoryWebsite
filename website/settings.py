36dd4264772a20007297bb7dc0d17febbb71ddc5
parkerg16 <parker.goins2016@gmail.com>
2024-02-16 12:27:33 -0500

    Set allowed hosts before adding to azure

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9si-slq$uf0@a6+$7d7&c@%j*%8u&4govord#_oe!v_e#@ix6_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
